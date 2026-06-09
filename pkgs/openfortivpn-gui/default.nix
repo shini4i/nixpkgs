f2d712bfc777a2dd1b6fb3801f02d746d670f8ed
Vadim Gedz <github@shini4i.dev>
2026-06-09 21:16:42 +0300

    chore(openfortivpn-gui): bump version to 0.3.2 (#26)
    - Updated version to 0.3.2
    - Updated rev to v0.3.2
    - Updated hash
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- pkgs/openfortivpn-gui/default.nix
+++ pkgs/openfortivpn-gui/default.nix
@@ -22,13 +22,13 @@
 
 buildGoModule rec {
   pname = "openfortivpn-gui";
-  version = "0.3.1";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
     owner = "shini4i";
     repo = "openfortivpn-gui";
-    rev = "v0.3.1";
-    hash = "sha256-istBZd5twbskw4JRl6G3V0lmAjBhllRUelxw1Otky74=";
+    rev = "v0.3.2";
+    hash = "sha256-cKf6TCI3jsrZ026wL1ZPxwmpLI78uIopk2yB439/lL0=";
   };
 
   vendorHash = "sha256-REBq9xL2ybU+cGAvTTkyFiv29y9T56f/i+YN7eDTTMk=";
